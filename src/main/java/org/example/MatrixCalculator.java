3f6c3e4fd39379d2f6b14a9d5dec5df61012d2df
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-10-09 11:50:20 +0300

    Implement MatrixCalculator constructor

--- src/main/java/org/example/MatrixCalculator.java
+++ src/main/java/org/example/MatrixCalculator.java
@@ -1,3 +1,19 @@
 package org.example;
 
-public class MatrixCalculator { }
+public class MatrixCalculator {
+    private final int rows;
+    private final int columns;
+
+    public MatrixCalculator() {
+        this.rows = 3;
+        this.columns = 3;
+    }
+
+    public MatrixCalculator(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new IllegalArgumentException("Rows and columns must be greater than 0");
+        }
+        this.rows = rows;
+        this.columns = columns;
+    }
+}
