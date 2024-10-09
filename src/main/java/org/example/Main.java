799bab8d80ba861cc357a65a213f90a335b7e5ad
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-10-09 11:55:44 +0300

    Add usage example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+
+        MatrixCalculator matrixCalculator = new MatrixCalculator(5, 5);
+        long[][] matrix = matrixCalculator.generateRandomMatrix();
+
+        System.out.println("Згенерована матриця:");
+        matrixCalculator.printMatrix(matrix);
+
+        long[][] transposedMatrix = matrixCalculator.transpose(matrix);
+        System.out.println("Транспонована матриця:");
+        matrixCalculator.printMatrix(transposedMatrix);
+
+        long sum = matrixCalculator.sumOfMinInColumns(transposedMatrix);
+        System.out.println("Сума найменших елементів у кожному стовпці транспонованої матриці: " + sum);
+    }
 }
